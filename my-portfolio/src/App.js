fb1f0a8f39c26e3e1bdc9c00f61509f1f2beea62
ebront08 <ebront08@gmail.com>
2021-11-01 10:35:08 -0400

    app.js additions

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,4 +1,20 @@
 import React from "react";
+// src/App.js
+
+import React from "react";
+
+export default function App() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
 
 export default function App() {
   return "App";
